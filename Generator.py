663f76b970c7a3209a3a39121169a87b76e3afb8
MU94W <nanqiao15@126.com>
2017-05-28 22:30:39 +0800

    model: add 'predict' method

--- Generator.py
+++ Generator.py
@@ -15,9 +15,11 @@ class G(object):
             inp: A tensor, with shape (batch_size, time_steps, feat_map[1])
         """
         with tf.variable_scope(scope or self.name, reuse=reuse):
-            assert inp.shape[1:].is_fully_defined(), "[!] Time dim and feat dim must be defined."
+            assert inp.shape[1:].is_fully_defined(), \
+                    "[!] Time dim and feat dim must be defined."
             feat_map = inp.shape.with_rank(3).as_list()[-1]
-            assert feat_map == feature_map_lst[0], "[!] input's last dim must be 1."
+            assert feat_map == feature_map_lst[0], \
+                    "[!] input's last dim must be 1."
 
             ### encode stage
             with tf.variable_scope("encode"):
@@ -29,17 +31,20 @@ class G(object):
                         in_channles = feature_map_lst[i-1]
                         this_filter = tf.get_variable(name="filter", \
                                 shape=(filter_width, in_channles, out_channles))
-                        this_out = tf.contrib.keras.layers.PReLU()(tf.nn.conv1d(last_out, this_filter, stride, padding))
+                        this_out = tf.contrib.keras.layers.PReLU()(\
+                                tf.nn.conv1d(last_out, this_filter, stride, padding))
                         per_layer_out.append(this_out)
                         last_out = this_out
 
             ### decode stage
             with tf.variable_scope("decode"):
-                latent_z = tf.expand_dims(tf.random_normal(shape=tf.shape(per_layer_out[-1])), axis=1)
+                latent_z = tf.expand_dims(\
+                        tf.random_normal(shape=tf.shape(per_layer_out[-1])), axis=1)
                 last_out = latent_z
                 out_channel_lst = feature_map_lst[::-1][1:]
                 batch_size = tf.shape(latent_z)[0]
-                for out_channles, res_out, idx in zip(out_channel_lst, per_layer_out[::-1], range(1, len(feature_map_lst))):
+                for out_channles, res_out, idx in \
+                        zip(out_channel_lst, per_layer_out[::-1], range(1, len(feature_map_lst))):
                     with tf.variable_scope("stage_%d" % idx):
                         res_out = tf.expand_dims(res_out, axis=1)
                         last_out_conc = tf.concat([last_out, res_out], axis=-1)
@@ -47,8 +52,13 @@ class G(object):
                         this_filter = tf.get_variable(name="filter", \
                                 shape=(1, filter_width, out_channles, in_channles))
                         output_shape = (batch_size, 1, 2*last_out_conc.shape[2].value, out_channles)
-                        this_out = tf.contrib.keras.layers.PReLU()(tf.reshape(tf.nn.conv2d_transpose(last_out_conc, this_filter, \
-                                output_shape=output_shape, strides=[1, 1, stride, 1], padding=padding), shape=output_shape))
+                        this_out = tf.contrib.keras.layers.PReLU()(\
+                                tf.reshape(\
+                                tf.nn.conv2d_transpose(last_out_conc, this_filter, \
+                                output_shape=output_shape, \
+                                strides=[1, 1, stride, 1], \
+                                padding=padding), \
+                                shape=output_shape))
                         last_out = this_out
 
             enhanced_out = tf.reshape(last_out, shape=tf.shape(inp))
--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from Generator import G
 from Discriminator import D
 
 learning_rate = 0.0002
-l1_lambda = 200
+l1_lambda = 100
 max_time_steps = 16384
 
 class SEGAN(Model):
@@ -15,26 +15,64 @@ class SEGAN(Model):
         self.__name = name
         self.g_nn = G()
         self.d_nn = D()
-        self.g_opt = tf.train.RMSPropOptimizer(learning_rate)
-        self.d_opt = tf.train.RMSPropOptimizer(learning_rate)
-        self.l1_lambda = l1_lambda
-        self.forward_done = False
-        self.backprop_done = False
-        self.build_done = False
+        self.__forward_done = False
+        self.__backprop_done = False
+        self.__build_done = False
 
     @property
     def name(self):
         return self.__name
 
+    @property
+    def clean_gtruth(self):
+        return self.__clean_gtruth
+
+    @property
+    def noisy_signal(self):
+        return self.__noisy_signal
+
+    @property
+    def enhenced_signal(self):
+        return self.__enhenced_signal
+
+    @property
+    def gtruth_score(self):
+        return self.__gtruth_score
+
+    @property
+    def fake_score(self):
+        return self.__fake_score
+
     def build(self, scope=None):
         """
         """
-        assert not self.build_done, "[!] have built model."
-        self.clean_gtruth = tf.placeholder(name="clean_or_enhenced", shape=(None, max_time_steps, 1), dtype=tf.float32)
-        self.noisy_signal = tf.placeholder(name="noisy_signal", shape=(None, max_time_steps, 1), dtype=tf.float32)
+        assert not self.__build_done, "[!] have built model."
         with tf.variable_scope(scope or "model"):
+            with tf.variable_scope("data"):
+                self.__clean_gtruth = tf.placeholder(name="clean_or_enhenced", \
+                        shape=(None, max_time_steps, 1), dtype=tf.float32)
+                self.__noisy_signal = tf.placeholder(name="noisy_signal", \
+                        shape=(None, max_time_steps, 1), dtype=tf.float32)
             self.__build_forward(self.clean_gtruth, self.noisy_signal)
             self.__build_backprop()
+            self.__build_done = True
+
+    def predict(self, data, sess, get_score=True):
+        """
+        """
+        noisy_signal = data.get("noisy_signal")
+        _, time_steps, channels = noisy_signal.shape
+        assert channels == 1, "[!] The input channel must be 1."
+        assert time_steps == max_time_steps, \
+                "[!] The input time_steps must be %d." % max_time_steps
+        feed_dict={self.noisy_signal: noisy_signal}
+        if get_score:
+            enhenced_signal_eval, fake_score_eval = \
+                    sess.run([self.enhenced_signal, self.fake_score], feed_dict)
+            return enhenced_signal_eval, fake_d_loss_eval
+        else:
+            enhenced_signal_eval = sess.run(self.enhenced_signal, feed_dict)
+            return enhenced_signal_eval
 
     def fit(self, data, sess, batch_size=10, epochs=10):
         """
@@ -45,7 +83,8 @@ class SEGAN(Model):
                 "[!] The shape of clean_gtruth and noisy_signal is not compatible."
         samples, time_steps, channels = clean_gtruth.shape
         assert channels == 1, "[!] The input channel must be 1."
-        assert time_steps == max_time_steps, "[!] The input time_steps must be %d." % max_time_steps
+        assert time_steps == max_time_steps, \
+                "[!] The input time_steps must be %d." % max_time_steps
         for epoch in range(epochs):
             print("Epoch %d / %d" % (epoch, epochs))
             perm = np.random.permutation(samples)
@@ -64,10 +103,11 @@ class SEGAN(Model):
         """
         """
         ### update D.
-        real_d_loss_eval, fake_d_loss_eval, *_ = sess.run([self.real_d_loss, self.fake_d_loss, self.d_upd], feed_dict)
+        real_d_loss_eval, fake_d_loss_eval, *_ = \
+                sess.run([self.__real_d_loss, self.__fake_d_loss, self.__d_upd], feed_dict)
         d_loss_eval = real_d_loss_eval + fake_d_loss_eval
         ### update G.
-        g_loss_eval, *_ = sess.run([self.g_loss, self.g_upd], feed_dict)
+        g_loss_eval, *_ = sess.run([self.__g_loss, self.__g_upd], feed_dict)
         ### show some useful info.
         console_log = "\r[%d / %d]:\td_loss: %f; g_loss: %f; real_d_loss: %f; fake_d_loss: %f" \
                 % (used_samples_cnt, total_samples, d_loss_eval, g_loss_eval, real_d_loss_eval, fake_d_loss_eval)
@@ -76,29 +116,29 @@ class SEGAN(Model):
     def __build_forward(self, clean_gtruth, noisy_signal, scope=None):
         """
         """
-        assert not self.forward_done, "[!] have built forward."
+        assert not self.__forward_done, "[!] have built forward."
         with tf.variable_scope(scope or "forward"):
             ### 1st. use clean_gtruth and noisy_signal to get gtruth_score.
-            gtruth_score = self.d_nn(tf.concat([clean_gtruth, noisy_signal], axis=-1), reuse=False)
+            self.__gtruth_score = self.d_nn(tf.concat([clean_gtruth, noisy_signal], axis=-1), reuse=False)
 
             ### 2nd. use noisy_signal to get enhenced_signal, and then cal the fake_score.
-            enhenced_signal = self.g_nn(noisy_signal, reuse=False)
-            fake_score = self.d_nn(tf.concat([enhenced_signal, noisy_signal], axis=-1), reuse=True)
+            self.__enhenced_signal = self.g_nn(noisy_signal, reuse=False)
+            self.__fake_score = self.d_nn(tf.concat([self.__enhenced_signal, self.__noisy_signal], axis=-1), reuse=True)
 
             ### 3rd. cal the real_d_loss, fake_d_loss, and fake_g_loss.
-            self.real_d_loss = tf.reduce_mean(tf.squared_difference(gtruth_score, 1.))
-            self.fake_d_loss = tf.reduce_mean(tf.squared_difference(fake_score, 0.))
-            self.fake_g_loss = tf.reduce_mean(tf.squared_difference(fake_score, 1.)) + \
-                    self.l1_lambda * tf.reduce_mean(tf.abs(tf.subtract(enhenced_signal, clean_gtruth)))
-            self.d_loss = self.real_d_loss + self.fake_d_loss
-            self.g_loss = self.fake_g_loss
-            self.forward_done = True
+            self.__real_d_loss = tf.reduce_mean(tf.squared_difference(self.__gtruth_score, 1.))
+            self.__fake_d_loss = tf.reduce_mean(tf.squared_difference(self.__fake_score, 0.))
+            self.__fake_g_loss = tf.reduce_mean(tf.squared_difference(self.__fake_score, 1.)) + \
+                    l1_lambda * tf.reduce_mean(tf.abs(tf.subtract(self.__enhenced_signal, self.__clean_gtruth)))
+            self.__d_loss = self.__real_d_loss + self.__fake_d_loss
+            self.__g_loss = self.__fake_g_loss
+            self.__forward_done = True
 
     def __build_backprop(self, scope=None):
         """
         """
-        assert self.forward_done, "[!] must run build_forward method first."
-        assert not self.backprop_done, "[!] have built backprop."
+        assert self.__forward_done, "[!] must run build_forward method first."
+        assert not self.__backprop_done, "[!] have built backprop."
         with tf.variable_scope(scope or "backprop"):
             ### get G and D 's trainable vars.
             train_vars = tf.trainable_variables()
@@ -106,13 +146,16 @@ class SEGAN(Model):
             d_vars = [var for var in train_vars if self.d_nn.name in var.name]
 
             ### get G and D 's grads and apply update.
-            d_opt = tf.train.RMSPropOptimizer(learning_rate)
-            g_opt = tf.train.RMSPropOptimizer(learning_rate)
-            d_grads = d_opt.compute_gradients(self.d_loss, d_vars)
-            g_grads = g_opt.compute_gradients(self.g_loss, g_vars)
+            with tf.variable_scope("optimizer"):
+                d_opt = tf.train.RMSPropOptimizer(learning_rate)
+                g_opt = tf.train.RMSPropOptimizer(learning_rate)
+                d_grads = d_opt.compute_gradients(self.__d_loss, d_vars)
+                g_grads = g_opt.compute_gradients(self.__g_loss, g_vars)
 
-            self.d_upd = d_opt.apply_gradients(d_grads)
-            self.g_upd = g_opt.apply_gradients(g_grads)
-            self.backprop_done = True
+                self.__d_upd = d_opt.apply_gradients(d_grads)
+                self.__g_upd = g_opt.apply_gradients(g_grads)
+
+            self.__backprop_done = True
+
 
 
