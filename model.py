665e744bb1b4d9475f35ee03152c61da769a4c4d
MU94W <nanqiao15@126.com>
2017-05-28 16:40:43 +0800

    SEGAN: add 'fit' method

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
-import tensorflow as tf
+import tensorflow as tf, numpy as np
 from TFCommon.Model import Model
 from Generator import G
 from Discriminator import D
 
 learning_rate = 0.0002
 l1_lambda = 200
+max_time_steps = 16384
 
 class SEGAN(Model):
     """
@@ -25,24 +26,52 @@ class SEGAN(Model):
     def name(self):
         return self.__name
 
-    def build(self, clean_gtruth, noisy_signal, scope=None):
+    def build(self, scope=None):
         """
         """
         assert not self.build_done, "[!] have built model."
+        self.clean_gtruth = tf.placeholder(name="clean_or_enhenced", shape=(None, max_time_steps, 1), dtype=tf.float32)
+        self.noisy_signal = tf.placeholder(name="noisy_signal", shape=(None, max_time_steps, 1), dtype=tf.float32)
         with tf.variable_scope(scope or "model"):
-            self.__build_forward(clean_gtruth, noisy_signal)
+            self.__build_forward(self.clean_gtruth, self.noisy_signal)
             self.__build_backprop()
 
-    def train(self, feed_dict, sess):
+    def fit(self, data, sess, batch_size=10, epochs=10):
         """
         """
-        ### update D
+        clean_gtruth = data.get("clean_gtruth")
+        noisy_signal = data.get("noisy_signal")
+        assert clean_gtruth.shape == noisy_signal.shape, \
+                "[!] The shape of clean_gtruth and noisy_signal is not compatible."
+        samples, time_steps, channels = clean_gtruth.shape
+        assert channels == 1, "[!] The input channel must be 1."
+        assert time_steps == max_time_steps, "[!] The input time_steps must be %d." % max_time_steps
+        for epoch in range(epochs):
+            print("Epoch %d / %d" % (epoch, epochs))
+            perm = np.random.permutation(samples)
+            start = 0
+            end = start + batch_size
+            while start < samples:
+                perm_index = perm[start:end]
+                feed_dict = {self.clean_gtruth: clean_gtruth[perm_index], \
+                        self.noisy_signal: noisy_signal[perm_index]}
+                self.train(min(end, samples), samples, feed_dict, sess)
+                start = end
+                end = start + batch_size
+            print("")
+
+    def train(self, used_samples_cnt, total_samples, feed_dict, sess):
+        """
+        """
+        ### update D.
         real_d_loss_eval, fake_d_loss_eval, *_ = sess.run([self.real_d_loss, self.fake_d_loss, self.d_upd], feed_dict)
         d_loss_eval = real_d_loss_eval + fake_d_loss_eval
-        ### update G
+        ### update G.
         g_loss_eval, *_ = sess.run([self.g_loss, self.g_upd], feed_dict)
-        print("d_loss: %f; g_loss: %f" % (d_loss_eval, g_loss_eval))
-        print("real_d_loss: %f; fake_d_loss: %f" % (real_d_loss_eval, fake_d_loss_eval))
+        ### show some useful info.
+        console_log = "\r[%d / %d]:\td_loss: %f; g_loss: %f; real_d_loss: %f; fake_d_loss: %f" \
+                % (used_samples_cnt, total_samples, d_loss_eval, g_loss_eval, real_d_loss_eval, fake_d_loss_eval)
+        print(console_log, end="")
 
     def __build_forward(self, clean_gtruth, noisy_signal, scope=None):
         """
